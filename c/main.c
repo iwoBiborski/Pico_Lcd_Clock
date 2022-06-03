e115510bf58383f66fc60714e7d5541f0042030b
iwoBiborski <iwo.biborski@gmail.com>
2022-06-03 13:42:56 +0200

    initial version of LCD cock

---
+++ c/main.c
@@ -0,0 +1,12 @@
+﻿#include "LCD_Test.h"   //Examples
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main(void)
+{
+    stdio_init_all();
+    sleep_ms(3000);  
+    printf("Welcome \n");  
+    LCD_2in_test();
+    return 0;
+}
